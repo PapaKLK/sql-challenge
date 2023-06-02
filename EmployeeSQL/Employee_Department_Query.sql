cd9dea536dfe8dc4e30f7357baee79010011dda6
PapaKLK <kjkrause1323@gmail.com>
2023-06-02 09:55:52 -0500

    adding files for 5

--- EmployeeSQL/Employee_Department_Query.sql
+++ EmployeeSQL/Employee_Department_Query.sql
@@ -5,4 +5,4 @@ SELECT E."Employee_ID",E."first_name", E."last_name",DE."Employee_ID", D."Depart
         ON E."Employee_ID" = DE."Employee_ID"
 		INNER JOIN "Department" D
 		ON DE."Department_ID" = D."Department_ID"
-ORDER BY E."Employee_ID"		
+		
